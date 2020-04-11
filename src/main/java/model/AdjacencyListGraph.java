59d1c1ba756e64eaa21a05edf60dec93a09ee991
Aryan Falahatpisheh <aryanfalahat@gmail.com>
2020-04-11 01:30:10 -0700

    # Separate Graph Structure and Node/Edge Labels
    The existing API was a graph which held different types of Nodes/Edges. Different implementations of Nodes/Edges would store different data. For example, for a chess game, a BoardTile could store a coordinate and which piece is on this tile (i.e. Queen, C8).
    Now, Node and Edge are concrete classes and not meant to be overridden. They are now only used to store information about the graph structure (i.e. which nodes connect to which, and in what direction?) They do not store any domain-specific data.
    To store data about nodes and edges, we will now use LabeledGraph instead. This design has some advantages:
        1. Many operations, such as getting a node's neighbors or performing Depth First Search, only depend on graph structure. Now, their classes can be defined more simply in terms of Node and Edge objects, instead of parameterized "N implements Node" and "E implements Edge" types.
        2. LabeledGraph allows us to store multiple layers of information about nodes/edges of a graph on top of a common network. This prevents duplicate data and reduces the risk of data inconsistency. If the original graph's structure is updated, the LabeledGraph will be automatically updated.
        3. Developers do not need to implement Node or Edge, and can instead quickly create a graph and work with its structure without implementing any extra classes.
        4. Makes tests simpler (No more parameterized GraphTest/NodeTest/EdgeTest)

--- src/main/java/model/AdjacencyListGraph.java
+++ src/main/java/model/AdjacencyListGraph.java
@@ -7,58 +7,82 @@ import model.node.Node;
 import java.util.*;
 import java.util.stream.Collectors;
 
-public class AdjacencyListGraph<N extends Node, E extends Edge<N>> implements Graph<N, E> {
-    private Map<N, List<E>> nodesToEdges;
+public class AdjacencyListGraph<N, E> implements Graph<N, E> {
+    private Map<Node, List<Edge>> nodesToEdges;
+    private Map<N, Node> nodeKeyToNode;
 
     public AdjacencyListGraph() {
         nodesToEdges = new HashMap<>(); // Contains both incoming + outgoing connections
+        nodeKeyToNode = new HashMap<>();
     }
 
     @Override
-    public boolean contains(N node) {
+    public boolean contains(Node node) {
         return nodesToEdges.containsKey(node);
     }
 
     @Override
-    public void add(N node) {
-        if (contains(node)) {
+    public Node add(N nodeKey) {
+        if (nodeKeyToNode.containsKey(nodeKey)) {
             throw new IllegalStateException("Node already exists");
         }
+
+        Node node = Node.withNodeId(nodeKey);
+        nodeKeyToNode.put(nodeKey, node);
         nodesToEdges.put(node, new ArrayList<>());
+        return node;
     }
 
     @Override
-    public void add(E edge) {
-        nodesToEdges.putIfAbsent(edge.node1(), new ArrayList<>());
-        nodesToEdges.putIfAbsent(edge.node2(), new ArrayList<>());
+    public Edge connect(Node source, Node destination) {
+        Edge edge = new Edge(source, destination);
+        nodesToEdges.putIfAbsent(source, new ArrayList<>());
+        nodesToEdges.putIfAbsent(destination, new ArrayList<>());
 
-        nodesToEdges.get(edge.node1()).add(edge);
-        nodesToEdges.get(edge.node2()).add(edge);
+        nodesToEdges.get(source).add(edge);
+        nodesToEdges.get(destination).add(edge);
+
+        return edge;
     }
 
     @Override
-    public void remove(N node) {
+    public void remove(Node node) {
         if (!contains(node)) {
             throw new IllegalStateException("Node does not exist");
         }
-        List<E> edges = nodesToEdges.remove(node);
-        edges.forEach(this::remove);
+
+        List<Edge> edges = nodesToEdges.remove(node);
+        edges.forEach(edge -> disconnect(edge.source(), edge.destination()));
     }
 
     @Override
-    public void remove(E edge) {
-        if (!nodesToEdges.get(edge.node1()).remove(edge) || !nodesToEdges.get(edge.node2()).remove(edge)) {
-            throw new IllegalStateException("Edge does not exist for at least one of the nodes");
-        }
+    public void disconnect(Node node1, Node node2) {
+        Edge edge = edge(node1, node2);
+        this.nodesToEdges.get(node1).remove(edge);
+        this.nodesToEdges.get(node2).remove(edge);
     }
 
     @Override
-    public Collection<N> nodes() {
-        return nodesToEdges.keySet();
+    public List<Node> nodes() {
+        return new ArrayList<>(nodesToEdges.keySet());
     }
 
     @Override
-    public Collection<E> edges() {
+    public List<Node> nodes(N targetValue) {
+        return new ArrayList<>(nodesToEdges.keySet());
+    }
+
+    @Override
+    public Edge edge(Node node1, Node node2) {
+        return nodesToEdges.get(node1).stream()
+                .filter(e -> e.source().equals(node2) || e.destination().equals(node2))
+                .findAny()
+                .orElseThrow(() -> new IllegalStateException("Tried to get a non-existent edge"));
+        // TODO: convert this API to lenient optional
+    }
+
+    @Override
+    public List<Edge> edges() {
         return nodesToEdges.values()
                 .stream()
                 .flatMap(Collection::stream)
@@ -66,7 +90,12 @@ public class AdjacencyListGraph<N extends Node, E extends Edge<N>> implements Gr
     }
 
     @Override
-    public Collection<E> edges(N node) {
+    public List<Edge> edges(E targetValue) {
+        return null;
+    }
+
+    @Override
+    public List<Edge> edges(Node node) {
         return nodesToEdges.get(node);
     }
 }
